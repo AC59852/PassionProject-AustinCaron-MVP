523ae1df07ccdd9d5438ff235ddb0d5cd5a59a07
AC59852 <austin.caron1@gmail.com>
2022-06-03 22:13:20 -0400

    Updated Everything!

---
+++ Migrations/202206031933477_characterfont.cs
@@ -0,0 +1,18 @@
+﻿namespace PassionProject_AustinCaron_MVP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class characterfont : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Characters", "CharacterFontRatio", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Characters", "CharacterFontRatio");
+        }
+    }
+}
