1f570c2229e2ba263270b34e1c5c9a4bb391c016
AC59852 <austin.caron1@gmail.com>
2022-06-15 14:54:32 -0400

    reverting some changes made with regard to image upload

---
+++ Migrations/202206151835595_animeImageTest.cs
@@ -0,0 +1,20 @@
+﻿namespace PassionProject_AustinCaron_MVP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class animeImageTest : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Animes", "AnimeHasPic", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Animes", "PicExtension", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Animes", "PicExtension");
+            DropColumn("dbo.Animes", "AnimeHasPic");
+        }
+    }
+}
---
+++ Migrations/202206151850011_animeImageRevert.cs
@@ -0,0 +1,20 @@
+﻿namespace PassionProject_AustinCaron_MVP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class animeImageRevert : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Animes", "AnimeHasPic");
+            DropColumn("dbo.Animes", "PicExtension");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Animes", "PicExtension", c => c.String());
+            AddColumn("dbo.Animes", "AnimeHasPic", c => c.Boolean(nullable: false));
+        }
+    }
+}
