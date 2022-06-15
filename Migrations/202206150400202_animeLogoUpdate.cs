a28f60da75d1500bbdca73b6ad6414cf0e503a3a
AC59852 <austin.caron1@gmail.com>
2022-06-15 04:44:47 -0400

    Added responsive design for each page. Hid edit pages on mobile/tablet.

---
+++ Migrations/202206150400202_animeLogoUpdate.cs
@@ -0,0 +1,22 @@
+﻿namespace PassionProject_AustinCaron_MVP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class animeLogoUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Animes", "AnimeHasLogo", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Animes", "LogoExtension", c => c.String());
+            DropColumn("dbo.Animes", "AnimeLogo");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Animes", "AnimeLogo", c => c.String());
+            DropColumn("dbo.Animes", "LogoExtension");
+            DropColumn("dbo.Animes", "AnimeHasLogo");
+        }
+    }
+}
---
+++ Migrations/202206150429138_animeRevert.cs
@@ -0,0 +1,22 @@
+﻿namespace PassionProject_AustinCaron_MVP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class animeRevert : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Animes", "AnimeLogo", c => c.String());
+            DropColumn("dbo.Animes", "AnimeHasLogo");
+            DropColumn("dbo.Animes", "LogoExtension");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Animes", "LogoExtension", c => c.String());
+            AddColumn("dbo.Animes", "AnimeHasLogo", c => c.Boolean(nullable: false));
+            DropColumn("dbo.Animes", "AnimeLogo");
+        }
+    }
+}
